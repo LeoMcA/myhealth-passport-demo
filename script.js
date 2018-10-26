0a0705e25ec0c2e082bf0032779d56ff96747276
Leo McArdle <leomcardle@gmail.com>
2018-10-26 09:13:46 +0100

    add app_page param

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let params = new URLSearchParams(window.location.search)
+let app_page = params.get("app_page")
+if (app_page) {
+  document.querySelector(".phone .screen").src = "app/" + app_page
+}
+
 document.querySelector(".commands button").addEventListener("click", () => {
   document.querySelector(".phone .screen").contentWindow.postMessage("hi", "*")
 })
